79aff6368802bac6bda128f108f35208e63251a3
Hadassa Fentanes <hadassafentanes@gmail.com>
2024-03-24 22:20:24 +0100

    Primeiros exercícios

---
+++ src/exercises/done/e01.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.languages = exports.isSummer = exports.population = exports.currency = exports.visited = exports.averageTemperature = exports.year = exports.country = void 0;
+// START: Follow the instructions below.
+// Add type annotations to each of these variables.
+var country = "Italy";
+exports.country = country;
+var year = undefined;
+exports.year = year;
+var averageTemperature = 23.6;
+exports.averageTemperature = averageTemperature;
+var visited = true;
+exports.visited = visited;
+var currency = null;
+exports.currency = currency;
+// Change the type annotations on these variables so they are correct.
+var population = 47450795;
+exports.population = population;
+var isSummer = false;
+exports.isSummer = isSummer;
+var languages = "Spanish, English";
+exports.languages = languages;
---
+++ src/exercises/done/e01.ts
@@ -0,0 +1,24 @@
+// START: Follow the instructions below.
+
+// Add type annotations to each of these variables.
+const country: string = "Italy";
+const year: undefined = undefined;
+const averageTemperature: number = 23.6;
+const visited: boolean = true;
+const currency: null = null;
+
+// Change the type annotations on these variables so they are correct.
+const population: number = 47_450_795;
+const isSummer: boolean = false;
+const languages: string = "Spanish, English";
+
+export {
+  country,
+  year,
+  averageTemperature,
+  visited,
+  currency,
+  population,
+  isSummer,
+  languages,
+};
